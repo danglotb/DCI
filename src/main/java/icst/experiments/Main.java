ce7644152be3cd94b3db39f000fbcbc41a2f6cdb
danglotb <bdanglot@gmail.com>
2018-08-30 11:29:34 +0200

    pom: add basic structure for java project

---
+++ src/main/java/icst/experiments/Main.java
@@ -0,0 +1,14 @@
+package icst.experiments;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 30/08/18
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Another Hello World!");
+    }
+
+}
