dc630e0e27dcc8c9f52b8441dcc775d00d400bc9
danglotb <bdanglot@gmail.com>
2018-09-03 18:56:47 +0200

    feat: more functionnalities in the toolbox to run the exp

--- src/main/python/september-2018/toolbox.py
+++ src/main/python/september-2018/toolbox.py
@@ -17,7 +17,15 @@ output_log_path = ""
 prefix_url_github = "https://github.com/"
 prefix_result = "result/september-2018/"
 
+path_to_jar = "target/experiments-0.1-SNAPSHOT-jar-with-dependencies.jar"
+full_qualified_name_repositories_setter = "icst.experiments.repositories.RepositoriesSetter"
+
 suffix_parent = "_parent"
+
+name_of_csv_with_list_of_test_that_execute_the_changes = ""
+
+def get_path_to_csv_file(project, commit_index):
+    return get_absolute_path(prefix_dataset + project + "/" + name_of_csv_with_list_of_test_that_execute_the_changes + "_" + commit_index + ".csv")
 
 def delete_if_exists(path):
     if os.path.isdir(path):
@@ -34,8 +42,25 @@ def get_absolute_path(path_file):
 
 
 def get_json_file(file_path):
+    if not file_path.endswith(".json"):
+        file_path = file_path + ".json"
     with open(file_path) as data_file:
         return json.load(data_file)
+
+def print_and_call_no_redirection(cmd, cwd=None):
+    if not cwd is None:
+        print cwd
+    print " ".join(cmd)
+    subprocess.call(cmd, cwd=cwd)
+
+def print_and_call_in_a_file_no_redirection(cmd, cwd=None):
+    with open(path_to_script_to_run, "w") as f:
+        cmd_to_execute = " ".join(cmd)
+        print cmd_to_execute
+        f.write(cmd_to_execute)
+        f.close()
+    subprocess.call(path_to_script_to_run, cwd=cwd, shell=True)
+
 
 def print_and_call(cmd, cwd=None):
     if not cwd is None:
@@ -44,7 +69,7 @@ def print_and_call(cmd, cwd=None):
     p1 = Popen(cmd, stdout=PIPE, cwd=cwd)
     p2 = Popen(["tee", "-a", output_log_path], stdin=p1.stdout, stdout=PIPE, stderr=PIPE, cwd=cwd)
     print p2.communicate()[0]
-
+    return p2.returncode
 
 path_to_script_to_run = get_absolute_path("src/main/bash/script.sh")
 
