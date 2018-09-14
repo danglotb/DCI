e30fc683732f19598cd605d4ff8b62afd9c530ef
danglotb <benjamin.danglot@inria.fr>
2018-09-14 09:47:47 +0200

    set to false to skip clover in xwiki-commons-tools

--- src/main/python/september-2018/preparation.py
+++ src/main/python/september-2018/preparation.py
@@ -13,6 +13,9 @@ xml_configuration_tag = xml_prefix + 'configuration'
 xml_includes_test_source_roots_tag = xml_prefix + 'includesTestSourceRoots'
 xml_properties_tag = xml_prefix + 'properties'
 xml_xwiki_enforcer_skip_tag = xml_prefix + 'xwiki.enforcer.skip'
+xml_xwiki_clover_skip_tag = xml_prefix + 'xwiki.clover.skip'
+
+xwiki_commons_tools_path = "xwiki-commons-tools/"
 
 def prepare(project):
     if project == "xwiki-commons":
@@ -20,11 +23,13 @@ def prepare(project):
 
 def set_true_include_test_roots(project):
     ET.register_namespace('', xml_namespace)
+    # set in top pom enforcer skip to true
     path_to_root_pom = toolbox.prefix_dataset + "/" + project + "/pom.xml"
     tree = ET.parse(path_to_root_pom)
     root_pom = tree.getroot()
     root_pom.findall(xml_properties_tag)[0].findall(xml_xwiki_enforcer_skip_tag)[0].text = "true"
     current_xml = root_pom.findall(xml_build_tag)[0].findall(xml_plugin_management_tag)[0].findall(xml_plugins_tag)[0]
+    # include test code into the clover instrumentation
     for child in current_xml:
         group_id = child.findall(xml_group_id_tag)[0].text
         artifact_id = child.findall(xml_artifact_id_tag)[0].text
@@ -32,11 +37,27 @@ def set_true_include_test_roots(project):
             child.findall(xml_configuration_tag)[0].findall(xml_includes_test_source_roots_tag)[0].text = "true"
     tree.write(path_to_root_pom)
 
+    # set in the xwiki-commons-tools pom to not skip clover in the submodule
+    path_to_root_pom = toolbox.prefix_dataset + "/" + project + "/" + xwiki_commons_tools_path + "/pom.xml"
+    tree = ET.parse(path_to_root_pom)
+    root_pom = tree.getroot()
+    root_pom.findall(xml_properties_tag)[0].findall(xml_xwiki_clover_skip_tag)[0].text = "false"
+    tree.write(path_to_root_pom)
+
+    # set in the second version enforcer skip to true
     path_to_root_pom = toolbox.prefix_dataset + "/" + project + toolbox.suffix_parent + "/pom.xml"
     tree = ET.parse(path_to_root_pom )
     root_pom = tree.getroot()
     root_pom.findall(xml_properties_tag)[0].findall(xml_xwiki_enforcer_skip_tag)[0].text = "true"
     tree.write(path_to_root_pom)
+
+    # set in the xwiki-commons-tools pom to not skip clover in the submodule for the second version
+    path_to_root_pom = toolbox.prefix_dataset + "/" + project + toolbox.suffix_parent + "/pom.xml"
+    tree = ET.parse(path_to_root_pom)
+    root_pom = tree.getroot()
+    root_pom.findall(xml_properties_tag)[0].findall(xml_xwiki_clover_skip_tag)[0].text = "false"
+    tree.write(path_to_root_pom)
+
 
 def add_needed_options(cmd, project):
     if project == "xwiki-commons":
