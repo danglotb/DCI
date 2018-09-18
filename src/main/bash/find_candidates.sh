9a9ef74c9f9ec091e694e086e8c849a085502090
danglotb <benjamin.danglot@inria.fr>
2018-09-18 23:12:29 +0200

    add logging

--- src/main/bash/find_candidates.sh
+++ src/main/bash/find_candidates.sh
@@ -3,5 +3,6 @@
 project=${1}
 owner=${2}
 goal=${3}
+log=result/september-2018/${project}/test_selection.log
 
-java -cp target/experiments-0.1-SNAPSHOT-jar-with-dependencies.jar icst.experiments.commits.ProjectJSONBuilder --path-to-repository dataset/${project} --owner ${owner} --project ${project} --output dataset/september-2018/ --size-goal ${goal}
+java -cp target/experiments-0.1-SNAPSHOT-jar-with-dependencies.jar icst.experiments.commits.ProjectJSONBuilder --path-to-repository dataset/${project} --owner ${owner} --project ${project} --output dataset/september-2018/ --size-goal ${goal} 2&>1 | tee -a ${log}
