46fb610c8701dc1bedfe2d6269ba2337a7b6bb6b
danglotb <benjamin.danglot@inria.fr>
2018-09-20 10:14:13 +0200

    fix install python script

--- src/main/python/september-2018/install.py
+++ src/main/python/september-2018/install.py
@@ -3,30 +3,25 @@ import sys
 import toolbox
 
 
-def install(project):
+def install(owner, project):
     toolbox.set_output_log_path(
-        toolbox.get_absolute_path(toolbox.get_absolute_path(toolbox.prefix_result + project + "_install.log")))
-
-    with open(toolbox.prefix_current_dataset + project + '.json') as data_file:
-        data = json.load(data_file)
-
-    toolbox.print_and_call_in_a_file(
-        " ".join(["git", "clone",
-                  toolbox.prefix_url_github + data["owner"] + "/" + data["name"], toolbox.prefix_dataset + project])
+        toolbox.prefix_result + project + "_install.log"
     )
 
     toolbox.print_and_call_in_a_file(
         " ".join(["git", "clone",
-         toolbox.prefix_url_github + data["owner"] + "/" + data["name"],
-         toolbox.prefix_dataset + project + toolbox.suffix_parent])
+                  toolbox.prefix_url_github + owner + "/" + project, toolbox.prefix_dataset + project]
+                 )
     )
 
+    toolbox.print_and_call_in_a_file(
+        " ".join(["git", "clone",
+                  toolbox.prefix_url_github + owner + "/" + project,
+                  toolbox.prefix_dataset + project + toolbox.suffix_parent]
+                 )
+    )
 
 if __name__ == '__main__':
-
-    if len(sys.argv) < 2 or "all" == sys.argv[1]:
-        projects = ["xwiki-commons"]
-        for project in projects:
-            install(project)
-    else:
-        install(sys.argv[1])
+    owner = sys.argv[1]
+    project = sys.argv[2]
+    install(owner, project)
