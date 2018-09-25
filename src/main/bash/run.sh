253bd53caf870cd285fe65401cc0229ca448811a
danglotb <benjamin.danglot@inria.fr>
2018-09-25 20:07:51 +0200

    fix and improve script

--- src/main/bash/run.sh
+++ src/main/bash/run.sh
@@ -4,5 +4,7 @@ project=${1}
 owner=${2}
 goal=${3}
 
-./find_candidates.sh ${project} ${owner} ${goal}
-python python src/main/python/september-2018/run-pre-selected.py ${project} 0 ${goal} onClusty
+python src/main/python/september-2018/install ${owner} ${project}
+./src/main/bash/find_candidates.sh ${project} ${owner} ${goal}
+python src/main/python/september-2018/run-pre-selected.py ${project} 0 ${goal} onClusty
+python src/main/python/september-2018/run-pre-selected.py ${project} 0 ${goal} onClusty amplifiers
