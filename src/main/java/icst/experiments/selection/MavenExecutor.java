e6a157f11eac018556c2d2e69be9c17f3cef5aa3
danglotb <benjamin.danglot@inria.fr>
2018-09-26 11:12:43 +0200

    add compiler properties in the maven goal execution, improve logging

--- src/main/java/icst/experiments/selection/MavenExecutor.java
+++ src/main/java/icst/experiments/selection/MavenExecutor.java
@@ -1,11 +1,13 @@
 package icst.experiments.selection;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.maven.shared.invoker.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
@@ -20,9 +22,22 @@ public class MavenExecutor {
 
     public static String mavenHome = "/home/spirals/danglot/apache-maven-3.5.4/";
 
+    private static final String[] additionnalProperties = new String[]{
+            "-Dmaven.compiler.source=1.8",
+            "-Dmaven.compiler.target=1.8",
+            "-Dmaven.compile.target=1.8",
+            "-Dmaven.compile.source=1.8",
+            "-Dcheckstyle.skip=true",
+            "-Denforcer.skip=true",
+            "-Dxwiki.clirr.skip=true"
+    };
+
     public static int runGoals(String pathToPom, String... goals) {
         InvocationRequest request = new DefaultInvocationRequest();
-        request.setGoals(Arrays.asList(goals));
+
+        final List<String> finalGoals = Arrays.asList(goals);
+        finalGoals.addAll(Arrays.asList(additionnalProperties));
+        request.setGoals(Arrays.asList(finalGoals));
         request.setPomFile(new File(pathToPom));
         request.setJavaHome(new File(System.getProperty("java.home")));
 
@@ -39,7 +54,7 @@ public class MavenExecutor {
 
         Invoker invoker = new DefaultInvoker();
         invoker.setMavenHome(new File(mavenHome));
-        LOGGER.info(String.format("run maven %s", Arrays.stream(goals).collect(Collectors.joining(" "))));
+        LOGGER.info("run {}/mvn {}", mavenHome, String.join(" ", finalGoals));
         invoker.setOutputHandler(System.out::println);
         invoker.setErrorHandler(System.err::println);
         try {
