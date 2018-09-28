04246e73ab01dce2db030118b61610ec013fadf1
danglotb <benjamin.danglot@inria.fr>
2018-09-28 23:03:46 +0200

    add jsoup input ampl 0 5

---
+++ september-2018/result/jsoup/commit_4_c8c0569/input_amplification/org/jsoup/helper/AmplDataUtilTest.java
@@ -0,0 +1,181 @@
+package org.jsoup.helper;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import org.jsoup.Jsoup;
+import org.jsoup.integration.ParseTest;
+import org.jsoup.nodes.Document;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AmplDataUtilTest {
+    private InputStream stream(String data) {
+        return new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private InputStream stream(String data, String charset) {
+        try {
+            return new ByteArrayInputStream(data.getBytes(charset));
+        } catch (UnsupportedEncodingException e) {
+            Assert.fail();
+        }
+        return null;
+    }
+
+    @Test(timeout = 10000)
+    public void supportsBOMinFileslitString90351_failAssert126() throws IOException, Exception {
+        try {
+            File in = ParseTest.getFile("/bomtests/bom_utf16be.html");
+            Document doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16BE");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf16le.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf32be.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32BE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            org.junit.Assert.fail("supportsBOMinFileslitString90351 should have thrown FileNotFoundException");
+        } catch (FileNotFoundException expected) {
+            Assert.assertEquals("/tmp/icst-2019/september-2018/dataset/jsoup_parent/target/test-classes/org/jsoup/integration (Is a directory)", expected.getMessage());
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void supportsBOMinFileslitString90243_failAssert128() throws IOException, Exception {
+        try {
+            File in = ParseTest.getFile("");
+            Document doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16BE");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf16le.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf32be.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32BE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf32le.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            org.junit.Assert.fail("supportsBOMinFileslitString90243 should have thrown FileNotFoundException");
+        } catch (FileNotFoundException expected) {
+            Assert.assertEquals("/tmp/icst-2019/september-2018/dataset/jsoup_parent/target/test-classes/org/jsoup/integration (Is a directory)", expected.getMessage());
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void supportsBOMinFileslitString90315_failAssert129() throws IOException, Exception {
+        try {
+            File in = ParseTest.getFile("/bomtests/bom_utf16be.html");
+            Document doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16BE");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf16le.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32BE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf32le.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            org.junit.Assert.fail("supportsBOMinFileslitString90315 should have thrown FileNotFoundException");
+        } catch (FileNotFoundException expected) {
+            Assert.assertEquals("/tmp/icst-2019/september-2018/dataset/jsoup_parent/target/test-classes/org/jsoup/integration (Is a directory)", expected.getMessage());
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void supportsBOMinFileslitString90279_failAssert127() throws IOException, Exception {
+        try {
+            File in = ParseTest.getFile("/bomtests/bom_utf16be.html");
+            Document doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16BE");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-16LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf32be.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32BE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            in = ParseTest.getFile("/bomtests/bom_utf32le.html");
+            doc = Jsoup.parse(in, null, "http://example.com");
+            doc.title().contains("UTF-32LE");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            doc.text().contains("??????");
+            org.junit.Assert.fail("supportsBOMinFileslitString90279 should have thrown FileNotFoundException");
+        } catch (FileNotFoundException expected) {
+            Assert.assertEquals("/tmp/icst-2019/september-2018/dataset/jsoup_parent/target/test-classes/org/jsoup/integration (Is a directory)", expected.getMessage());
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void supportsUTF8BOMlitString59683_failAssert73() throws IOException, Exception {
+        try {
+            File in = ParseTest.getFile("");
+            Document doc = Jsoup.parse(in, null, "http://example.com");
+            doc.head().select("title").text();
+            org.junit.Assert.fail("supportsUTF8BOMlitString59683 should have thrown FileNotFoundException");
+        } catch (FileNotFoundException expected) {
+            Assert.assertEquals("/tmp/icst-2019/september-2018/dataset/jsoup_parent/target/test-classes/org/jsoup/integration (Is a directory)", expected.getMessage());
+        }
+    }
+
+    @Test(timeout = 10000)
+    public void supportsUTF8BOM_add59707litString60906_failAssert100() throws IOException, Exception {
+        try {
+            File in = ParseTest.getFile("");
+            Document o_supportsUTF8BOM_add59707__3 = Jsoup.parse(in, null, "http://example.com");
+            Document doc = Jsoup.parse(in, null, "http://example.com");
+            String o_supportsUTF8BOM_add59707__6 = doc.head().select("title").text();
+            org.junit.Assert.fail("supportsUTF8BOM_add59707litString60906 should have thrown FileNotFoundException");
+        } catch (FileNotFoundException expected) {
+            Assert.assertEquals("/tmp/icst-2019/september-2018/dataset/jsoup_parent/target/test-classes/org/jsoup/integration (Is a directory)", expected.getMessage());
+        }
+    }
+}
+
