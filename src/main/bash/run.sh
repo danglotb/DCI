b5d89de57e854520cc7a46cafbca92a73d687936
danglotb <benjamin.danglot@inria.fr>
2018-09-28 23:20:11 +0200

    remove old result experiments

--- src/main/bash/run.sh
+++ src/main/bash/run.sh
@@ -6,5 +6,6 @@ goal=${3}
 
 python src/main/python/september-2018/install.py ${owner} ${project}
 ./src/main/bash/find_candidates.sh ${project} ${owner} ${goal}
+python src/main/python/september-2018/diff_coverage.py ${project} onClusty
 python src/main/python/september-2018/run-pre-selected.py ${project} 0 ${goal} onClusty
 python src/main/python/september-2018/run-pre-selected.py ${project} 0 ${goal} onClusty amplifiers
