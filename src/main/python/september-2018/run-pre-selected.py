c7c77557c205f36257dbac7384df1dca6d42a5a9
danglotb <benjamin.danglot@inria.fr>
2018-10-06 19:31:36 +0200

    remove some input amplification

--- src/main/python/september-2018/run-pre-selected.py
+++ src/main/python/september-2018/run-pre-selected.py
@@ -47,7 +47,7 @@ def run(project, index_begin, index_end, amplifiers):
         ]
         if amplifiers:
             cmd.append(
-                "-Damplifiers=TestDataMutator,MethodAdd,MethodRemove,MethodGeneratorAmplifier,ReturnValueAmplifier,NullifierAmplifier")
+                "-Damplifiers=TestDataMutator,MethodAdd,MethodRemove,NullifierAmplifier")
             cmd.append("-Diteration=3")
             cmd.append("-Dbudgetizer=SimpleBudgetizer")
         cmd = preparation.add_needed_options(cmd, project)
