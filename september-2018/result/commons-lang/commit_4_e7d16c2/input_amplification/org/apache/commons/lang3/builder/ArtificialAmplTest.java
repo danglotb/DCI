75756f7c5a7de914557f57e827cefd8dd1c199af
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2018-10-15 13:43:51 +0200

    Update ArtificialAmplTest.java

--- september-2018/result/commons-lang/commit_4_e7d16c2/input_amplification/org/apache/commons/lang3/builder/ArtificialAmplTest.java
+++ september-2018/result/commons-lang/commit_4_e7d16c2/input_amplification/org/apache/commons/lang3/builder/ArtificialAmplTest.java
@@ -8,7 +8,7 @@ public void testChar_add45986_literalMutationChar46277() throws Exception {
   ToStringBuilder o_testChar_add45986__10 = 
     new ToStringBuilder(this.base).append("a", '\n').append("b", 'B');
   Assert.assertEquals("{\"a\":\"\n\",\"b\":\"B\",", 
-    ((StringBuffer) (((ToStringBuilder) (o_testChar_add45986__10)).getStringBuffer())).toString());
+    ((StringBuffer) o_testChar_add45986__10.getStringBuffer()).toString());
   Assert.assertEquals("{\"a\":\"\n\",\"b\":\"B\"}", ((ToStringBuilder) (o_testChar_add45986__10)).toString());
   Assert.assertEquals(5, ((int) (((ToStringBuilder) (o_testChar_add45986__10)).getObject())));
   new ToStringBuilder(this.base).append("a", 'A').append("b", 'B').toString();
